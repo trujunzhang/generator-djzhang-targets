76564f8b1c2c75921f50629a6b45e2bd90f8c91e
trujunzhang <trujunzhang@gmail.com>
2016-07-12 17:06:29 +0800

    generator.

--- app/templates/spider/database_factory.py
+++ app/templates/spider/database_factory.py
@@ -1,6 +1,3 @@
-from cw<%= appname%>.parser.browse_parser import BrowseParser
-from cw<%= appname%>.parser.response_parser import ResponseParse
-
 from enum import Enum
 
 
@@ -10,27 +7,35 @@ class CollectionTypes(Enum):
     item = 3
 
 
-class DatabaseFactory:
-    def __init__(self):
-        pass
+class DatabaseFactory(object):
+    def __init__(self, host, port, user, passwd, db, collection_name):
+        super(DatabaseFactory, self).__init__()
+        self.host = host
+        self.port = port
+        self.user = user
+        self.passwd = passwd
+        self.db = db
+        self.collection_name = collection_name
 
-    # This is the factory method
-    @staticmethod
-    def get_database(dbType, uri, db="vps_scrapy_rails", collection="<%= appname%>s"):
-
-        from cw<%= appname%>.database.cache_db import CacheDatabase
-        from cw<%= appname%>.database.history_db import HistoryDatabase
-        from cw<%= appname%>.database.item_db import ItemDatabase
-
-        if CollectionTypes.cache == dbType:
-            database = CacheDatabase(uri, db + "_cache", "_cache_" + collection)
+    def get_database(self, collection_type):
+        if CollectionTypes.cache == collection_type:
+            from cw<%= appname%>.database.cache_db import CacheDatabase
+            database = CacheDatabase(host=self.host, port=self.port,
+                                     user=self.user, passwd=self.passwd,
+                                     db=self.db, collection_name=self.collection_name + '_caches')
             database.open_spider()
             return database
-        elif CollectionTypes.history == dbType:
-            history_database = HistoryDatabase(uri, db + "_history", "_history_" + collection)
+        elif CollectionTypes.history == collection_type:
+            from cw<%= appname%>.database.history_db import HistoryDatabase
+            history_database = HistoryDatabase(host=self.host, port=self.port,
+                                               user=self.user, passwd=self.passwd,
+                                               db=self.db, collection_name=self.collection_name + "_histories")
             history_database.open_spider()
             return history_database
-        elif CollectionTypes.item == dbType:
-            return ItemDatabase(uri, db, collection)
+        elif CollectionTypes.item == collection_type:
+            from cw<%= appname%>.database.item_db import ItemDatabase
+            return ItemDatabase(host=self.host, port=self.port,
+                                user=self.user, passwd=self.passwd,
+                                db=self.db, collection_name=self.collection_name)
         else:
             return None
