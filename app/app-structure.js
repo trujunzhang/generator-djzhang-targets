1a15785797a481dba122def3d5cadbc9d80f684a
trujunzhang <trujunzhang@gmail.com>
2016-07-19 21:50:15 +0800

    change template by cwpoliticl( with wordpress).

--- app/app-structure.js
+++ app/app-structure.js
@@ -1,8 +1,78 @@
 'use strict';
 
+
+var spiders = {
+  "source": "/spider/spiders",
+  "dest": "/" + projectName + "/spiders",
+  "value": [
+    "app_browser_spider.py",
+    "app_spider.py",
+    "app_browser_debug_spider.py",
+    "app_debug_spider.py"
+  ],
+  "renames": [
+    this.appname + "_browser_spider.py",
+    this.appname + "_spider.py",
+    this.appname + "_browser_debug_spider.py",
+    this.appname + "_debug_spider.py"
+  ]
+};
+var files = [
+  {
+    "source": "",
+    "dest": "",
+    "value": [
+      "readme.MD",
+      "scrapy.cfg",
+      "setup.py"
+    ]
+  },
+  {
+    "source": "/spider",
+    "dest": "/" + projectName,
+    "value": [
+      "database_factory.py",
+      "items.py",
+      "main.py",
+      "settings.py"
+    ]
+  },
+  {
+    "source": "/spider/tests",
+    "dest": "/" + projectName + "/tests",
+    "value": [
+      "test_mysql_db.py"
+    ]
+  },
+  {
+    "source": "/spider/database",
+    "dest": "/" + projectName + "/database",
+    "value": [
+      "cache_db.py",
+      "history_db.py",
+      "item_db.py"
+    ]
+  },
+  {
+    "source": "/spider/database/base",
+    "dest": "/" + projectName + "/database/base",
+    "value": [
+      "mongo_db.py",
+      "mysql_db.py"
+    ]
+  },
+  {
+    "source": "/spider/parser",
+    "dest": "/" + projectName + "/parser",
+    "value": [
+      "base_parser.py",
+      "browse_parser.py",
+      "response_parser.py"
+    ]
+  },
+  spiders
+];
 module.exports = {
-  server: "localhost",
-  port: 1234,
-  timeout: 10,
-  wanghao:'djzhang-123'
+  spiders: spiders,
+  files: files
 };
--- app/index.js
+++ app/index.js
@@ -10,7 +10,6 @@ module.exports = generators.Base.extend({
   _createProjectFileSystem: function () {
     var destRoot = this.destinationRoot(),
         sourceRoot = this.sourceRoot(),
-        appDir = destRoot,
         templateContext = {
           appname: this.appname,
           appclassname: this.appclassname,
@@ -19,97 +18,22 @@ module.exports = generators.Base.extend({
         };
     var projectName = "/cw" + this.appname;
 
-    mkdirp(appDir + projectName);
-    mkdirp(appDir + projectName + projectName);
+    var destProjectRoot = destRoot + projectName;
 
-    this.fs.copy(this.sourceRoot() + "/*", destRoot + projectName);
-    this.fs.copy(this.sourceRoot() + "/.gitignore", destRoot + projectName + "/.gitignore");
-    this.fs.copy(this.sourceRoot() + "/debug/*", destRoot + projectName + "/debug");
-    this.fs.copy(this.sourceRoot() + "/spider/**/*", destRoot + projectName + projectName);
+    this.fs.copy(this.sourceRoot() + "/*/**", destProjectRoot);
+    this.fs.copy(this.sourceRoot() + "/.gitignore", destProjectRoot + "/.gitignore");
 
-    var spiders = {
-      "source": "/spider/spiders",
-      "dest": "/" + projectName + "/spiders",
-      "value": [
-        "app_browser_spider.py",
-        "app_spider.py",
-        "app_browser_debug_spider.py",
-        "app_debug_spider.py"
-      ],
-      "renames": [
-        this.appname + "_browser_spider.py",
-        this.appname + "_spider.py",
-        this.appname + "_browser_debug_spider.py",
-        this.appname + "_debug_spider.py"
-      ]
-    };
-    var files = [
-      {
-        "source": "",
-        "dest": "",
-        "value": [
-          "readme.MD",
-          "scrapy.cfg",
-          "setup.py"
-        ]
-      },
-      {
-        "source": "/spider",
-        "dest": "/" + projectName,
-        "value": [
-          "database_factory.py",
-          "items.py",
-          "main.py",
-          "settings.py"
-        ]
-      },
-      {
-        "source": "/spider/tests",
-        "dest": "/" + projectName + "/tests",
-        "value": [
-          "test_mysql_db.py"
-        ]
-      },
-      {
-        "source": "/spider/database",
-        "dest": "/" + projectName + "/database",
-        "value": [
-          "cache_db.py",
-          "history_db.py",
-          "item_db.py"
-        ]
-      },
-      {
-        "source": "/spider/database/base",
-        "dest": "/" + projectName + "/database/base",
-        "value": [
-          "mongo_db.py",
-          "mysql_db.py"
-        ]
-      },
-      {
-        "source": "/spider/parser",
-        "dest": "/" + projectName + "/parser",
-        "value": [
-          "base_parser.py",
-          "browse_parser.py",
-          "response_parser.py"
-        ]
-      },
-      spiders
-    ];
-
-    files.forEach(function (entry) {
+    structure.files.forEach(function (entry) {
       var _value = entry.value;
       _value.forEach(function (file) {
-        this.fs.copyTpl(this.sourceRoot() + entry.source + "/" + file, destRoot + projectName + entry.dest + "/" + file, templateContext);
+        this.fs.copyTpl(this.sourceRoot() + entry.source + "/" + file, destProjectRoot + entry.dest + "/" + file, templateContext);
       }, this);
     }, this);
 
-    spiders.value.forEach(function (file, index) {
-      var rename = spiders.renames[index];
-      this.fs.move(destRoot + projectName + spiders.dest + "/" + file, destRoot + projectName + spiders.dest + "/" + rename);
-    }, this);
+    // spiders.value.forEach(function (file, index) {
+    //   var rename = spiders.renames[index];
+    //   this.fs.move(destProjectRoot + spiders.dest + "/" + file, destProjectRoot + spiders.dest + "/" + rename);
+    // }, this);
   },
 
   _getPrompts: function () {
